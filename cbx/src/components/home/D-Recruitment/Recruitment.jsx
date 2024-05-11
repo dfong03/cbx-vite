97c0b4cecf48ea049d5c0469aec7eb86f7d3f4e6
dfong03 <149826311+dfong03@users.noreply.github.com>
2024-05-11 12:09:32 -0400

    Update Recruitment.jsx

--- cbx/src/components/home/D-Recruitment/Recruitment.jsx
+++ cbx/src/components/home/D-Recruitment/Recruitment.jsx
@@ -38,7 +38,7 @@ const Recruitment = () => {
                             Operations Team Resume Drop
                             {/* Investment team resume*/}
                         </a>{" "}
-                        | Submit Interest
+                        | Submit
                     </span>
                 </div>
             </div>
