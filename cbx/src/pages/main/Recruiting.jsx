de646636c42a920c90d9f6721ea86be8ff7d4425
Bao To <tothienbao6a0@gmail.com>
2024-08-30 18:42:21 -0400

    up

--- cbx/src/pages/main/Recruiting.jsx
+++ cbx/src/pages/main/Recruiting.jsx
@@ -4,23 +4,28 @@ import RecruitmentForm from "../../components/home/E-Form/RecruitForm";
 const Recruiting = () => {
     return (
         <div className="top-0 left-0 w-screen">
-    <div className="w-screen h-[120vh] bg-black relative overflow-hidden flex flex-col items-center justify-center">
-        <div className="text-white h-screen w-screen absolute z-10 flex flex-col items-center justify-center sm:flex">
-            <div className="w-1/3 text-xl text-center">
-                <h1 className="font-bold mb-4 text-2xl">
-                    Recruitment
-                </h1>
-                <p>
-                    CBX Partners’ investment team does not accept unsolicited applications or resumes. Should you be referred, you will be approached by a member of the team. We seek Columbia undergrads with demonstrated aptitude, passion, and experience in investing.
-                </p>
-                
-                <p className="mt-8">
-                recruiting@cbxcapitalpartners.com
-                </p>
+        <div className="w-screen h-[120vh] bg-black relative overflow-hidden flex items-start justify-start">
+            <div className="text-white h-screen w-screen absolute z-10 flex flex-col items-start justify-start pl-8">
+                <div className="ml-16 w-1/3 text-xl">
+                    <h1 className="font-bold mt-32 text-6xl text-left" style={{ color: '#b4e0fa' }}>
+                        Recruitment
+                    </h1>
+                    <p className="mt-16 text-2xl text-left">
+                        CBX Partners’ investment team does not accept unsolicited applications or resumes. Should you be referred, you will be approached by a member of the team.
+                    </p>
+
+                    <p className="mt-4 text-2xl text-left">
+                        We seek Columbia undergrads with demonstrated aptitude, passion, and experience in investing.
+                    </p>
+                    
+                    <p className="mt-8 text-2xl text-left">
+                        recruiting@cbxcapitalpartners.com
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    
 
     );
 };
